63f0eaf12270ce4b7c5789b27489322a2b0b863c
cechus <cechuscsc4@gmail.com>
2020-04-14 23:31:27 -0400

    editado index en branchA (cechus)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.sass';
-import App from './App';
+import Main from './Main';
 
 ReactDOM.render(
-  <Router>
-    <App />
-<div>
-holaaa 
-</div>
-  </Router>,
-  document.getElementById('root')
+  <Main />
+  document.getElementById('main')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
