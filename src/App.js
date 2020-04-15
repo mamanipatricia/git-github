fb471cccafb9f55a5a5f50a37a207ac86b6edb43
mamanipatricia <patttymq@gmail.com>
2020-04-14 22:56:38 -0400

    modified index.js and app.js file

--- src/App.js
+++ src/App.js
@@ -4,16 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-        >
           Learn GIT
-        </a>
-      </header>
     </div>
   );
 }
