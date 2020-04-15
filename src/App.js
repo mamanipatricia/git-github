de19da6da0c6aad23d97fece157cdab1fbf36d6f
mamanipatricia <patttymq@gmail.com>
2020-04-14 23:28:20 -0400

    importing whatever in app.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import pandas
+import numpy
 
 function App() {
   return (
+    <div className="App">
     <h1>Hi</h1>
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+	<p>la tusa XD</p>
+	<span>IMPORTANDO CHUNKS</span>
+        </p>
+          Learning Git
+          Learn React
+      </header>
+    </div>
   );
 }
 
